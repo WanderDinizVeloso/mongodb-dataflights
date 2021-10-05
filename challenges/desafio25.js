67b6c27068e796135770a28ba67cf9cd2d59c327
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-10-05 14:22:31 -0300

    altera desafio 25

--- challenges/desafio25.js
+++ challenges/desafio25.js
@@ -1,7 +1,5 @@
 db.voos
-  .deleteMany(
-    { 
-      "empresa.name": "AZUL",
-      litrosCombustivel: { $lt: 400 },
-    },
-  );
+  .deleteMany({
+    "empresa.nome": "AZUL",
+    litrosCombustivel: { $lt: 400 },
+  });
